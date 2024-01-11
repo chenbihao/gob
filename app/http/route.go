91986a99e407ba89a055afbd8f529abbd54154e9
被水淹没 <994523036@qq.com>
2024-01-11 17:56:54 +0800

    引入static库、转发静态文件

--- app/http/route.go
+++ app/http/route.go
@@ -4,12 +4,16 @@ import (
 	"github.com/chenbihao/gob/app/http/module/demo"
 	"github.com/chenbihao/gob/framework/gin"
 	"github.com/chenbihao/gob/framework/middleware"
+	"github.com/chenbihao/gob/framework/middleware/static"
 )
 
 // Routes 绑定业务层路由
 func Routes(r *gin.Engine) {
 
 	r.Static("/dist/", "./dist/")
+
+	// /路径先去./dist目录下查找文件是否存在，找到使用文件服务提供服务
+	r.Use(static.Serve("/", static.LocalFile("./gob_frontend/dist", false)))
 
 	// 使用全链路追踪
 	r.Use(middleware.Trace())
---
+++ framework/middleware/static/static_test.go
@@ -0,0 +1,136 @@
+package static
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+
+	"github.com/chenbihao/gob/framework/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+// nolint:unparam
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequestWithContext(context.Background(), method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestEmptyDirectory(t *testing.T) {
+	// SETUP file
+	testRoot, _ := os.Getwd()
+	f, err := ioutil.TempFile(testRoot, "")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(f.Name())
+	_, _ = f.WriteString("Gin Web Framework")
+	f.Close()
+
+	dir, filename := filepath.Split(f.Name())
+
+	router := gin.New()
+	router.Use(ServeRoot("/", dir))
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "index")
+	})
+	router.GET("/a", func(c *gin.Context) {
+		c.String(http.StatusOK, "a")
+	})
+	router.GET("/"+filename, func(c *gin.Context) {
+		c.String(http.StatusOK, "this is not printed")
+	})
+	w := performRequest(router, "GET", "/")
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, w.Body.String(), "index")
+
+	w = performRequest(router, "GET", "/"+filename)
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, w.Body.String(), "Gin Web Framework")
+
+	w = performRequest(router, "GET", "/"+filename+"a")
+	assert.Equal(t, w.Code, 404)
+
+	w = performRequest(router, "GET", "/a")
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, w.Body.String(), "a")
+
+	router2 := gin.New()
+	router2.Use(ServeRoot("/static", dir))
+	router2.GET("/"+filename, func(c *gin.Context) {
+		c.String(http.StatusOK, "this is printed")
+	})
+
+	w = performRequest(router2, "GET", "/")
+	assert.Equal(t, w.Code, 404)
+
+	w = performRequest(router2, "GET", "/static")
+	assert.Equal(t, w.Code, 404)
+	router2.GET("/static", func(c *gin.Context) {
+		c.String(http.StatusOK, "index")
+	})
+
+	w = performRequest(router2, "GET", "/static")
+	assert.Equal(t, w.Code, http.StatusOK)
+
+	w = performRequest(router2, "GET", "/"+filename)
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, w.Body.String(), "this is printed")
+
+	w = performRequest(router2, "GET", "/static/"+filename)
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, w.Body.String(), "Gin Web Framework")
+}
+
+func TestIndex(t *testing.T) {
+	// SETUP file
+	testRoot, _ := os.Getwd()
+	f, err := os.Create(path.Join(testRoot, "index.html"))
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(f.Name())
+	_, _ = f.WriteString("index")
+	f.Close()
+
+	dir, filename := filepath.Split(f.Name())
+
+	router := gin.New()
+	router.Use(ServeRoot("/", dir))
+
+	w := performRequest(router, "GET", "/"+filename)
+	assert.Equal(t, w.Code, 301)
+
+	w = performRequest(router, "GET", "/")
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, w.Body.String(), "index")
+}
+
+func TestListIndex(t *testing.T) {
+	// SETUP file
+	testRoot, _ := os.Getwd()
+	f, err := ioutil.TempFile(testRoot, "")
+	if err != nil {
+		t.Error(err)
+	}
+	defer os.Remove(f.Name())
+	_, _ = f.WriteString("Gin Web Framework")
+	f.Close()
+
+	dir, filename := filepath.Split(f.Name())
+	router := gin.New()
+	router.Use(Serve("/", LocalFile(dir, true)))
+
+	w := performRequest(router, "GET", "/"+filename)
+	assert.Equal(t, w.Code, http.StatusOK)
+	assert.Equal(t, w.Body.String(), "Gin Web Framework")
+
+	w = performRequest(router, "GET", "/")
+	assert.Contains(t, w.Body.String(), `<a href="`+filename)
+}
