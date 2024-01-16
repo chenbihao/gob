c243c6ff9d8d9d2d8cac60806fc28c1afd303761
被水淹没 <994523036@qq.com>
2024-01-16 18:19:41 +0800

    源码引入swag、swagfiles、gzip等中间件，并且调整框架引入中间件包

---
+++ framework/middleware/gzip/example/example.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/chenbihao/gob/framework/gin"
+	"github.com/chenbihao/gob/framework/middleware/gzip"
+)
+
+func main() {
+	r := gin.Default()
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
+	})
+
+	// Listen and Server in 0.0.0.0:8080
+	if err := r.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
