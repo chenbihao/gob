8dfff18be695738e13e1808bcd418a2190f703bc
陈壁浩 <chenbihao@qljy.com>
2024-06-12 21:15:31 +0800

    syscall

--- framework/util/syscall_win.go
+++ framework/util/syscall_win.go
@@ -5,6 +5,8 @@ package util
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 // SetProcessTitle 设置进程名
@@ -15,20 +17,32 @@ func SetProcessTitle(name string) {
 
 // CheckProcessExist Will return true if the process with PID exists.
 func CheckProcessExist(pid int) bool {
-	// 查询这个pid
-	if _, err := os.FindProcess(pid); err != nil {
+	cmd := exec.Command("tasklist", "/FI", fmt.Sprintf("PID eq %d", pid))
+	output, err := cmd.Output()
+	if err != nil {
 		return false
 	}
-	return true
+
+	// 检查输出中是否包含进程pid
+	return strings.Contains(string(output), fmt.Sprintf("%d", pid))
 }
 
 // KillProcess
 func KillProcess(pid int) (err error) {
 	// 获取进程信息
-	p, err := os.FindProcess(pid)
-	if err != nil {
+	if _, err = os.FindProcess(pid); err != nil {
 		fmt.Printf("Failed to find process: %v\n", err)
+		return
+	}
+	return WinKillProcess(pid)
+}
+
+func WinKillProcess(pid int) error {
+	cmd := exec.Command("taskkill", "/F", "/PID", fmt.Sprintf("%d", pid))
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(out))
 		return err
 	}
-	return p.Kill()
+	return nil
 }
