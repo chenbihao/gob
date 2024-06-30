2eca2ff84b58be4bea28b5e402991a1e1272da82
陈壁浩 <chenbihao@qljy.com>
2024-06-30 16:44:11 +0800

    update todo

--- framework/util/word.go
+++ framework/util/word.go
@@ -1,14 +1,24 @@
 package word
 
-import "strings"
+import (
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"strings"
+)
+
+var caser = cases.Title(language.English)
+
+func ToTitle(word string) string {
+	return caser.String(word)
+}
 
 // ToTitleCamel 将下划线分割的字符串转换为驼峰字符串
 // class_id => ClassId
 func ToTitleCamel(word string) string {
 	// 将字符串分割为单词，并使用 Title 函数将每个单词的首字母大写
 	words := strings.Split(word, "_")
-	for i, word := range words {
-		words[i] = strings.Title(word)
+	for i, w := range words {
+		words[i] = ToTitle(w)
 	}
 	// 将单词连接在一起，形成一个驼峰字符串
 	camelCase := strings.Join(words, "")
@@ -20,12 +30,12 @@ func ToTitleCamel(word string) string {
 func ToNormalCamel(word string) string {
 	// 将字符串分割为单词，并使用 Title 函数将每个单词的首字母大写
 	words := strings.Split(word, "_")
-	for i, word := range words {
+	for i, w := range words {
 		if i == 0 {
-			words[i] = strings.ToLower(word)
+			words[i] = strings.ToLower(w)
 			continue
 		}
-		words[i] = strings.Title(word)
+		words[i] = ToTitle(w)
 	}
 	// 将单词连接在一起，形成一个驼峰字符串
 	camelCase := strings.Join(words, "")
